a0f4d3669e23a96fc72bb80339917d8bc0234d17
root <root@DESKTOP-DRLDR0N.localdomain>
2022-12-14 12:42:27 +0100

    ___

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python3
+
 def best_score(a_dictionary):
     for i in a_dictionary:
-        ans += a
+        if not i:
+            return None
+        else:
+            ans = a_dictionary[[0]]
+            if a_dictionary[i] > a_dictionary[ans]:
+                a_dictionary[ans] = a_dictionary[i]
+    return ans
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,3 +3,10 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     roman_key = {"I": 1, "V": 5, "X": 10, "L":50, "C": 100, "D": 500, "M": 1000}
+    answer = 0
+    for i in range(len(roman_key)):
+        if i > 0 and roman_key[roman_string[i]] < roman_key[roman_string[i + 1]]:
+            answer -= roman_key[roman_string[i]]
+        else:
+            answer += roman_key[roman_string[i]]
+    return answer
